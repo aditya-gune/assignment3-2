ec8ea628dcea26c6487f4135bb5a7bb6665307df
agune001 <ca_adi@yahoo.com>
2015-04-26 19:24:48 -0400

    fetchdata function

--- demo.js
+++ demo.js
@@ -1,10 +1,42 @@
 // JavaScript Document
 var OriginalGistList = [];
+var gistsArray = [];
 var fetchData = function(){
 	var req = new XMLHttpRequest();
-	if(!req){
-		throw 'Unable to create HttpRequest.';
-	}
+	var url = 'https://api.github.com/gists/public';
+	req.onreadystatechange = function(){ 
+		if(this.readyState === 4){
+			OriginalGistList = JSON.parse(this.responseText)
+			var params = {
+		 url: 'url',
+		forks_url: 'forks_url',
+		commits_url: 'commits_url',
+		id: 'id',
+		git_pull_url: 'git_pull_url',
+		git_push_url: 'git_push_url',
+		html_url: 'html_url',
+		files: 'files',
+		public: 'public',
+		created_at: 'created_at',
+		updated_at: 'updated_at',
+		description: 'description',
+		comments: 'comments',
+		user: 'user',
+		comments_url: 'comments_url'
+		
+	};
+			for (var i = 0; i < OriginalGistList.length; i++)
+			{
+				createGistList(document.getElementById('gists'),
+							 OriginalGistList);
+				
+			}
+			
+		
+		}
 
-
+};
+	req.open('GET', url);
+	req.send();
 }
+
